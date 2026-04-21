fad817917ed9f2970d70d4b3777755e5695a0226
shreyya18 <shreyapoojary251@gmail.com>
2026-04-21 22:18:36 +0530

    Add package.json for server with initial dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import UserRoute from './Modules/User/URoutes/UserRoute'
+import AdminRoute from './Modules/Admin/ARoutes/adminRoute'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+          <Routes>
+            <Route path='/*' element={<UserRoute/>}/>
+            <Route path='/Admin' element={<AdminRoute/>}/>
+
+  
+          </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/Modules/Admin/ARoutes/AdminRoute.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminRoute = () => {
+  return (
+    <div>adminRoute</div>
+  )
+}
+
+export default AdminRoute
